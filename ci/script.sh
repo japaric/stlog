9a6b9af970c81b4a50d0e1e5fe3c9b675d005405
Jorge Aparicio <jorge@japaric.io>
2019-01-27 16:46:53 +0100

    ci: test the spanned feature

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,25 @@ set -euxo pipefail
 
 main() {
     cargo check --all
+
+    if [ $TRAVIS_RUST_VERSION = nightly ]; then
+        cargo check --features spanned
+    fi
 }
+
+# fake Travis variables to be able to run this on a local machine
+if [ -z ${TRAVIS_RUST_VERSION-} ]; then
+    case $(rustc -V) in
+        *nightly*)
+            TRAVIS_RUST_VERSION=nightly
+            ;;
+        *beta*)
+            TRAVIS_RUST_VERSION=beta
+            ;;
+        *)
+            TRAVIS_RUST_VERSION=stable
+            ;;
+    esac
+fi
 
 main
